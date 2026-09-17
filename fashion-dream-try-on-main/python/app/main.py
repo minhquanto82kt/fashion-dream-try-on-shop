bb8daa6eab2620ff002a9a655ef4dc0db7cbc8a8
minhquanto82kt <tominhquan.kt@gmail.com>
2026-09-17 11:26:55 +0700

    feat(admin): register product tags API

--- fashion-dream-try-on-main/python/app/main.py
+++ fashion-dream-try-on-main/python/app/main.py
@@ -2,6 +2,7 @@
 
 from fastapi import FastAPI
 
+from app.api.admin_product_tags import router as admin_product_tags_router
 from app.api.admin_tags import router as admin_tags_router
 from app.api.health import router as health_router
 from app.api.images import router as images_router
@@ -20,6 +21,7 @@ app = FastAPI(
 app.include_router(health_router)
 app.include_router(products_router)
 app.include_router(admin_tags_router)
+app.include_router(admin_product_tags_router)
 app.include_router(images_router)
 app.include_router(try_on_router)
 app.include_router(product_vision_router)
